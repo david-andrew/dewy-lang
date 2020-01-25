e72cb856d938bf7a60f976db91d0f5ce4b149576
David A. Samson <david.andrew.engineer@gmail.com>
2020-01-25 14:47:07 -0500

    automatic update to docs on Sat Jan 25 14:47:07 EST 2020

---
+++ mdbook/autodocs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#serve the docs for modification, then git push when ctrl-c recieved on serving
+
+#push docs updates to git
+function git_push() {
+    git add ../docs ../mdbook
+    git commit -m "automatic update to docs on $(date)"
+    git push
+}
+
+#run git_push when ctrl-c received
+trap git_push INT
+ 
+#serve the docs at localhost:3000 and open a chrome tab there
+google-chrome http://localhost:3000 &
+mdbook serve
