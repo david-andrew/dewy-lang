849a260d3f692a24520633daeef62ecf324f7b2a
David A. Samson <david.andrew.engineer@gmail.com>
2020-02-05 04:01:01 -0500

    started adding functions for AST actions. also reworked functions to be more intuitive

--- dewy0alpha/parser.c
+++ dewy0alpha/parser.c
@@ -5,10 +5,11 @@
 #include <string.h>
 
 #include "object.c"
+#include "token.c"
 #include "vector.c"
 #include "dictionary.c"
 #include "set.c"
-
+#include "mast.c"
 #include "scanner.c"
 
 
@@ -29,53 +30,43 @@ int get_next_real_token(vect* tokens, int i);
 int get_next_token_type(vect* tokens, token_type type, int i);
 void update_meta_symbols(dict* meta_symbols, vect* tokens);
 void create_lex_rule(dict* meta_rules, vect* tokens);
+bool expand_rules(vect* tokens, dict* meta_rules);
+obj* build_ast(vect* tokens);
+size_t get_lowest_precedence_index(vect* tokens);
+size_t find_closing_pair(vect* tokens, size_t start);
 
 //returns the index of the next non-whitespace and non-comment token.
 //returns -1 if none are present in the vector
 int get_next_real_token(vect* tokens, int i)
 {
-    //the current token in the stream
-    token* t;
-
     //while we haven't reached the end of the token stream
-    //and the current token is whitespace or a comment
-    while (i < vect_size(tokens) && (t = (token*)vect_get(tokens, i)->data) && (t->type == whitespace || t->type == comment))
+    //if the current token isn't whitespace or a comment, return its index
+    while (i < vect_size(tokens))
     {
+        token* t = (token*)vect_get(tokens, i)->data;
+        if (t->type != whitespace && t->type != comment) { return i; }
         i++;
     }
 
-    if (i < vect_size(tokens))
-    {
-        return i;
-    }
-    else //reached end without finding a real token
-    {
-        return -1;
-    }
+    //reached end without finding a real token
+    return -1;
 }
 
 //return the index of the first occurance of the specified token type.
 //returns -1 if not present in the vector
 int get_next_token_type(vect* tokens, token_type type, int i)
 {
-    //the current token in the stream
-    token* t;
-
     //while we haven't reached the end of the tokens stream
-    //and the current token isn't the desired type
-    while (i < vect_size(tokens) && (t = (token*)vect_get(tokens, i)->data) && (t->type != type))
+    //if the current token is the desired type, return its index
+    while (i < vect_size(tokens))
     {
+        token* t = (token*)vect_get(tokens, i)->data;
+        if (t->type == type) { return i; }
         i++;
     }
 
-    if (i < vect_size(tokens))
-    {
-        return i;
-    }
-    else //reached end without finding token of desired type
-    {
-        return -1;
-    }
+    //reached end without finding token of desired type
+    return -1;
 }
 
 
@@ -151,6 +142,7 @@ void update_meta_symbols(dict* meta_symbols, vect* tokens)
     // obj_print(rule);
     // printf("\n");
 
+    //TODO->probably construct AST for rule here, and then store the AST in the meta symbol table
     dict_set(meta_symbols, id, rule);
     // printf("returned from dict_set\n");
     // dict_str(meta_symbols);
@@ -222,7 +214,90 @@ void create_lex_rule(dict* meta_rules, vect* tokens)
     //for now simply print out the rules to be lexed
     printf("Adding scanner rules: ");
     vect_str(lex_rules);
+
+    //TODO->determine if we expand rules greedily or lazily 
+    //for rule in lex_rules
+    //  create ast from expanded rule
+
     printf("\n");
 }
+
+
+/**
+    replace any instances of #rule with the 
+*/
+bool expand_rules(vect* tokens, dict* meta_rules)
+{
+    return false;
+}
+
+/**
+    Recursively construct an AST out of 
+*/
+obj* build_ast(vect* tokens)
+{
+    //if #rules in tokens, at current precedence level don't have memoized ASTs yet
+    //while (expand_rules(rule)){}
+
+    //get the index of the lowest precedence token
+    //if type is binary operator, build left and right trees from left and right side splits
+    //if type is unary group,
+    return NULL; 
+}
+
+/**
+    return the index of the token with the lowest precedence
+    if least precedence operator is a pair, e.g. [], {}, (), return the index of the left side
+*/
+size_t get_lowest_precedence_index(vect* tokens)
+{
+    //precedence levels
+    //groups: []  ()  {}
+    //
+
+    //keep var for level of precedence
+    //scan through tokens, determine token with minimum precedence level.
+    //need to skip paren/bracket/brace pairs (still note precedence level of opening)
+    //once have min precedence level, determine if left or right associative
+    //if left associative, find left-most? instance of operator with determined precedence level
+    //else (right associative) find right-most? instance of operator with determined precedence level 
+    return 0;
+}
+
+/**
+    return the index of the matching token pair for [], {}, ()
+    will return 0 if no matching pair found
+*/
+size_t find_closing_pair(vect* tokens, size_t start)
+{
+    obj* t = vect_get(tokens, start);
+    token_type opening = ((token*)t->data)->type;
+    token_type closing;
+    switch (opening) //determine matching closing type based on opening type
+    {
+        case meta_left_brace: { closing = meta_right_brace; break; }
+        case meta_left_bracket: { closing = meta_right_brace; break; }
+        case meta_left_parenthesis: { closing = meta_right_brace; break; }
+        default: { printf("ERROR: find_closing_pair() called with non-pair type token (%d)\n", opening); return 0; }
+    }
+    int stack = -1;
+    size_t stop = start + 1;
+    while (stop < vect_size(tokens))
+    {
+        obj* t_obj = vect_get(tokens, stop);
+        token* t = (token*)t_obj->data;
+        if (t->type == opening) { stack--; }
+        else if (t->type == closing) { stack++; }
+        if (stack == 0) { return stop; }
+        stop++;
+    }
+    
+    printf("ERROR: no matching pair found for token type (%d) in vector: ", opening);
+    vect_str(tokens);
+    printf("\n");
+    return 0;
+}
+
+
 
 #endif
