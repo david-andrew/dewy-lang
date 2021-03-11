c2a58a096dbeb1567e576b0178924fda7a0f9746
David Samson <david.andrew.engineer@gmail.com>
2021-03-10 23:15:42 -0500

    some more notes on metaparser

--- src/compiler/metaparser.h
+++ src/compiler/metaparser.h
@@ -1,4 +1,40 @@
 #ifndef METAPARSER_H
 #define METAPARSER_H
 
+
+#include "set.h"
+
+/**
+ * Productions are #head = #body
+ * -> head set
+ * -> body set
+ * -> production join table
+ * 
+ * 
+ * sequencing:
+ * - collect a rule together
+ * - convert all charset expressions to a single charset
+ * - split out all or's from the rule
+ * 
+ * 
+ * 
+ * Higher level sequencing
+ * -> item sets
+ * -> rnglr table
+ * ---> closure + goto + first
+ * 
+ * 
+ * 
+ * 
+ * interface Item {
+ *   head::UInt64,
+ *   body::UInt64,
+ *   position::UInt64,
+ *   lookahead::charset
+ * }
+ */
+
+
+
+
 #endif
