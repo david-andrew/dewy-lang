e919fe92be3e3771fc65b6b4ddaeb69117023131
David A. Samson <david.andrew.engineer@gmail.com>
2021-04-06 12:41:00 -0400

    added bool print parameters for compiler steps. cleaned up some comments

--- src/compiler/dewy_compiler_compiler.h
+++ src/compiler/dewy_compiler_compiler.h
@@ -9,11 +9,11 @@
 
 
 void run_compiler_compiler(char* source, bool verbose, bool scanner, bool ast, bool parser, bool grammar, bool table);
-void run_compiler(uint32_t* source);
+void run_compiler(uint32_t* source, bool compile, bool forest);
 void print_scanner(vect* tokens, bool verbose);
 void print_ast(uint64_t head_idx, metaast* body_ast, bool verbose);
 void print_parser(bool verbose);
-void print_grammar(bool verbose);
+void print_grammar();
 void print_raw_table();
 void print_table();
 
