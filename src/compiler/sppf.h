5cf05b24561647a61b26c14d847819f9f6d86864
David A. Samson <david.andrew.engineer@gmail.com>
2021-04-09 16:29:38 -0400

    started builing out sppf data strucutre

--- src/compiler/sppf.h
+++ src/compiler/sppf.h
@@ -3,5 +3,18 @@
 
 //shared packed parse forest data structure for storing the parse trees generated
 
+#include "vector.h"
+
+//nodes are represented by two types of lists. 
+//normal lists, list out the plain string that makes up the node, e.g. node1, node2, ... etc.
+//packed lists, represent current ambiguity, so they are lists of the aforementioned normal list
+
+//edges are mapped between tuples, since I think some node coordinates may be 1D while others may be 2D
+
+typedef struct {
+    vect* nodes; //vect<vect | pack>
+    dict* edges; //map<tuple, tuple>
+} sppf;
+
 
 #endif
