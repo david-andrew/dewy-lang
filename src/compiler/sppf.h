9891196abbfa957affbc9cd4042695d142879b58
David Samson <david.andrew.engineer@gmail.com>
2021-04-13 18:55:18 -0400

    more brainstorming on how to setup the sppf data structure

--- src/compiler/sppf.h
+++ src/compiler/sppf.h
@@ -4,65 +4,41 @@
 //shared packed parse forest data structure for storing the parse trees generated
 
 #include "vector.h"
-// #include "dictionary.h"
+#include "dictionary.h"
 #include "set.h"
 
-//nodes are represented by two types of lists. 
-//normal lists, list out the plain string that makes up the node, e.g. node1, node2, ... etc.
-//packed lists, represent current ambiguity, so they are lists of the aforementioned normal list
-
-//edges are mapped between tuples, since I think some node coordinates may be 1D while others may be 2D
-
-
-// dict* edge_node_map; //map<gss_edge, sppf_node> from GSS edges to nodes in the SPPF
-
-// typedef struct {
-//     uint64_t head_idx;
-//     uint64_t source_start_idx;
-//     uint64_t source_end_idx;
-//     vect* children;
-//     bool packed;
-// } sppf_node;
-
-// typedef enum {
-//     leaf,
-//     nullable,
-//     normal,
-//     packed
-//  } sppf_type;
-
-// typedef struct {
-//     sppf_type type;
-//     union {
-//         struct {} leaf;
-//         struct {} nullable;
-//         struct {} normal;
-//         struct {} packed;
-//     } node;
-// } sppf_node;
-
-
-
-dict* sppf_map; //map<sppf_node, sppf_child_idx | vect<sppf_child_idx>>
 
 typedef struct {
-    // set* nodes;     //set<tuple<uint64, uint64, uint64>>, where tuple is (head_idx, source_start_idx, source_end_idx)
-    // vect* children; //vect<vect<uint64> | vect<vect<uint64>> | NULL>, where outer vect has a 1-to-1 match with nodes, and inner vect can be either a list of the children nodes, or a packed node containing a list of lists of children nodes
-    //if children inner is null, then it is a leaf node, and the tuple in nodes contains the index of the input source terminal
-
-    // vect* nodes; //vect<sentence | pack | leaf>
-    // dict* edges; //map<tuple, tuple>
+    dict* nodes;    //map<sppf_node, child_list_idx | vect<child_list_idx> | NULL>      //where child_list_idx is uint64_t. NULL is for leaf and nullable nodes
+    set* children;  //set<vect<child_node_idx>>                                         //where child_node_idx is uint64_t
+    // dict* edge_node_map; //map<gss_edge, sppf_node_idx> from GSS edges to nodes in the SPPF
 } sppf;
 
+typedef enum {
+    leaf,
+    nullable,
+    inner,          //includes both normal and packed nodes
+ } sppf_type;
 
-//sentence is a an obj SPPFSentence_t, containing a vect*
-//pack is an obj SPPFPack_t, containing a vect*
-//leaf is a SPPFLeaf_t, containing a uint64_t pointing to the corresponding char
+typedef struct {
+    sppf_type type;
 
+    //normal and packed nodes have same node, just different children (handled by dict* nodes pointing to either a uint, or a vect<uint>)
+    //leaf nodes 
 
-// typedef struct {
-//     vect* alternates;
-// } pack;
+    //Depends on if metaast works with union version or not
+    // void* node;
+    // union {
+    //     uint64_t leaf;       //index in source of the terminal character
+    //     uint64_t nullable;   //index in the sppf_nodes dict of the specific nullable node
+    //     struct {
+    //         uint64_t head_idx, 
+    //         // uint64_t production_idx?,    //for future use
+    //         uint64_t source_start_idx, 
+    //         uint64_t source_end_idx
+    //     } inner;
+    // } node;
+} sppf_node;
 
 
 #endif
