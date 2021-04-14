d5cddc428cb6a6e7e9091ac18b3df74fb268eab2
David Samson <david.andrew.engineer@gmail.com>
2021-04-13 23:26:25 -0400

    wrote out basic structures/functions for sppf

--- src/compiler/sppf.c
+++ src/compiler/sppf.c
@@ -1,6 +1,108 @@
 #ifndef SPPF_C
 #define SPPF_C
 
+#include "sppf.h"
+
+
+/**
+ * 
+ */
+sppf* new_sppf()
+{
+
+}
+
+
+/**
+ * 
+ */
+void sppf_free(sppf* s)
+{
+
+}
+
+
+/**
+ * 
+ */
+sppf_node* new_sppf_leaf_node(uint64_t source_idx)
+{
+
+}
+
+
+/**
+ * 
+ */
+sppf_node* new_sppf_nullable_node(uint64_t nullable_idx)
+{
+
+}
+
+
+/**
+ * 
+ */
+sppf_node* new_sppf_inner_node(uint64_t head_idx, uint64_t source_start_idx, uint64_t source_end_idx)
+{
+
+}
+
+
+/**
+ * 
+ */
+obj* net_sppf_node_obj(sppf_node* n)
+{
+
+}
+
+
+/**
+ * 
+ */
+uint64_t sppf_node_hash(sppf_node* n)
+{
+
+}
+
+
+/**
+ * 
+ */
+bool sppf_node_equals(sppf_node* left, sppf_node* right)
+{
+
+}
+
+
+/**
+ * 
+ */
+void sppf_node_str(sppf_node* n)
+{
+
+}
+
+
+/**
+ * 
+ */
+void sppf_node_repr(sppf_node* n)
+{
+
+}
+
+
+/**
+ * 
+ */
+void sppf_node_free(sppf_node* n)
+{
+
+}
+
+
 
 
 #endif
--- src/compiler/sppf.h
+++ src/compiler/sppf.h
@@ -3,6 +3,7 @@
 
 //shared packed parse forest data structure for storing the parse trees generated
 
+#include "object.h"
 #include "vector.h"
 #include "dictionary.h"
 #include "set.h"
@@ -22,23 +23,33 @@ typedef enum {
 
 typedef struct {
     sppf_type type;
-
-    //normal and packed nodes have same node, just different children (handled by dict* nodes pointing to either a uint, or a vect<uint>)
-    //leaf nodes 
-
-    //Depends on if metaast works with union version or not
-    // void* node;
-    // union {
-    //     uint64_t leaf;       //index in source of the terminal character
-    //     uint64_t nullable;   //index in the sppf_nodes dict of the specific nullable node
-    //     struct {
-    //         uint64_t head_idx, 
-    //         // uint64_t production_idx?,    //for future use
-    //         uint64_t source_start_idx, 
-    //         uint64_t source_end_idx
-    //     } inner;
-    // } node;
+    union {
+        uint64_t leaf;       //index in source of the terminal character
+        uint64_t nullable;   //index in the sppf_nodes dict of the specific nullable node
+        struct {
+            uint64_t head_idx;
+            // uint64_t production_idx?;    //for future use
+            uint64_t source_start_idx; 
+            uint64_t source_end_idx;
+        } inner;
+    } node;
 } sppf_node;
+
+
+sppf* new_sppf();
+void sppf_free(sppf* s);
+
+sppf_node* new_sppf_leaf_node(uint64_t source_idx);
+sppf_node* new_sppf_nullable_node(uint64_t nullable_idx);
+sppf_node* new_sppf_inner_node(uint64_t head_idx, uint64_t source_start_idx, uint64_t source_end_idx);
+obj* net_sppf_node_obj(sppf_node* n);
+uint64_t sppf_node_hash(sppf_node* n);
+bool sppf_node_equals(sppf_node* left, sppf_node* right);
+void sppf_node_str(sppf_node* n);
+void sppf_node_repr(sppf_node* n);
+void sppf_node_free(sppf_node* n);
+
+
 
 
 #endif
