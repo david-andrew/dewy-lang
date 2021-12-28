131b889e598e619c815b2ea8c68182da7b5c9546
David Samson <david.andrew.engineer@gmail.com>
2021-12-28 15:42:41 -0500

    continued writing out rule110 llvm example

--- src/compiler/llvm-tests/c_examples/c_rule110.c
+++ src/compiler/llvm-tests/c_examples/c_rule110.c
@@ -1,7 +1,16 @@
+#include <stdint.h>
 #include <stdlib.h>
+
+// uint8_t* arr;
 
 int main()
 {
-    void* p = malloc(10);
-    free(p);
+    // arr = malloc(8);
+    int8_t arr[5];
+
+    uint8_t a = arr[2];
+    a = a | 0x01 << 3;
+    arr[2] = a;
+
+    // free(arr);
 }
