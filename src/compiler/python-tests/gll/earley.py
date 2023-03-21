e728342b2862aeecb0a0b1578ee704fbbc113cf5
David Samson <david.andrew.engineer@gmail.com>
2023-03-20 21:40:28 -0400

    broke out python grammar components to separate file

--- src/compiler/python-tests/gll/earley.py
+++ src/compiler/python-tests/gll/earley.py
@@ -3,3 +3,42 @@
 #test how fast it is compared to FUN-GLL parser
 #also compare how complicated the implementation is
 #once pick earley or fun-gll, last step is to figure out how to apply disambiguation filters, and any speedups from practical gll parsing
+
+
+"""
+Predictor((X ::= α · Y β, i), j)
+{
+    for all productions Y ::= γ
+    {
+        if(Y ::= ·γ, j) ∈ Sj
+        {
+            add (Y ::= ·γ, j) to Sj and to Rj 
+        }
+        for all (Y ::= δ·, j) ∈ Sj 
+        {
+            if(X ::= αY · β, j) ∈ Sj 
+            {
+                add (X ::= αY · β, j) to Sj and to Rj
+                bsrAdd(X ::= αY · β, i, j, j) 
+            } 
+        } 
+    }
+}
+
+Completer((X ::= α·, i), j){
+    for all (Y ::= δ · Xμ, k) ∈ Si {
+        if(Y ::= δX · μ, k) ∈ Sj {
+            add (Y ::= δX · μ, k) to Sj and to Rj
+            bsrAdd(X ::= α·, i, k, j) } }
+}
+
+Scanner((X ::= α · bβ, i), j){
+    if(X ::= αb · β, i) ∈ Sj+1 {
+        add (X ::= αb · β, i) to Sj+1 and to Rj+1
+        bsrAdd(X ::= αb · β, i, j, j + 1) }
+}
+
+bsrAdd((X ::= α · β, i, k, j) {
+    if(β = ){ insert (X ::= α, i, k, j) into Υ }
+    else if(|α| > 1){ insert (α, i, k, j) into Υ }}
+"""
--- src/compiler/python-tests/gll/fungll.py
+++ src/compiler/python-tests/gll/fungll.py
@@ -1,10 +1,12 @@
+from grammar import NonTerminal, Terminal, Sentence, Slot, Grammar
+
+
+
 #implementation of the functional GLL parsing process from https://pure.royalholloway.ac.uk/portal/files/35434658/Accepted_Manuscript.pdf
 #TODO: maybe look into the parsing combinators that they also discuss in the paper-->future work
 
 #TODO: replace tau:str with tau:Sequence[T] where T could be strings, or any other token type
 
-from abc import ABC
-from dataclasses import dataclass
 
 import pdb
 
@@ -12,68 +14,6 @@ import pdb
 # class MAST(ABC): ... #perhaps this should just be grammar? or something
 # class EBNF(ABC): ... #this is the higher level grammar with extra notation
 
-
-class Symbol(ABC): ...
-
-@dataclass(slots=True, frozen=True, eq=True)
-class Terminal(Symbol):
-    t: str
-    def __str__(self): return self.t
-
-@dataclass(slots=True, frozen=True, eq=True)
-class NonTerminal(Symbol):
-    X: str
-    def __str__(self): return self.X
-
-@dataclass(slots=True, frozen=True, eq=True)
-class Sentence:
-    symbols: tuple[Symbol, ...] = ()
-    def base_str(self): return " ".join(map(str, self.symbols))
-    def __str__(self): return self.base_str() if len(self.symbols) > 0 else "ϵ"
-    def __len__(self): return len(self.symbols)
-    def __getitem__(self, i: int|slice):
-        if isinstance(i, slice): return Sentence(self.symbols[i])
-        return self.symbols[i]
-
-class Grammar:
-    def __init__(self, rules:dict[NonTerminal, list[Sentence]]=None, start:NonTerminal=None):
-        self.rules = rules if rules else {}
-
-    def add_rule(self, X:NonTerminal, sentence:Sentence):
-        if X not in self.rules:
-            self.rules[X] = []
-        self.rules[X].append(sentence)
-
-    def __repr__(self):
-        return f'Grammar(start={self.start}, rules={self.rules})'
-
-    def __str__(self):
-        lines = []
-        for X in self.rules:
-            lines.append(f'{X} -> {" | ".join(map(str, self.rules[X]))}')
-
-        return '\n'.join(lines)
-
-
-@dataclass(slots=True, frozen=True, eq=True)
-class Slot:
-    X: NonTerminal
-    rule: Sentence
-    i: int
-    def __str__(self): return f'{self.X} ::= {self.alpha.base_str()}•{self.beta.base_str()}'
-    def __repr__(self): return f'Slot(X={self.X}, rule={self.rule}, i={self.i})'
-    @property
-    def alpha(self) -> Sentence: return self.rule[:self.i]
-    @property 
-    def beta(self) -> Sentence: return self.rule[self.i:]
-    @property
-    def betap(self) -> Sentence: return self.rule[self.i+1:]
-    @property
-    def s(self) -> Symbol: return self.rule[self.i]
-    def next(self) -> 'Slot': 
-        assert self.i < len(self.rule), f'Cannot get next slot for {self}. Slot is already at end of rule.'
-        return Slot(self.X, self.rule, self.i+1) 
-    
 
 Commencement = tuple[NonTerminal, int]      #(X:NonTerminal, l:int)
 Continuation = tuple[Slot, int]             #(g:Slot, l:int)
