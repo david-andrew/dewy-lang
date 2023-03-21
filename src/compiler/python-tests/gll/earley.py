2294271bd8cf2f831f326b5f177f117856529eb2
David Samson <david.andrew.engineer@gmail.com>
2023-03-20 20:59:39 -0400

    moved gll test programs into dedicated folder, and created stub for earley implementation

---
+++ src/compiler/python-tests/gll/earley.py
@@ -0,0 +1,5 @@
+#implement early parser from page 6 of Derivation representation using binary subtree sets
+#also implement conversion from BSR to SPPF
+#test how fast it is compared to FUN-GLL parser
+#also compare how complicated the implementation is
+#once pick earley or fun-gll, last step is to figure out how to apply disambiguation filters, and any speedups from practical gll parsing
