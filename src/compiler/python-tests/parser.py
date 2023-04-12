0cb1b73eadd59e7c268a7415de075d6b9f4fcfdb
David Samson <david.andrew.engineer@gmail.com>
2023-04-12 00:33:55 -0400

    write baseic tokenizing loop

--- src/compiler/python-tests/parser.py
+++ src/compiler/python-tests/parser.py
@@ -32,6 +32,7 @@ class WhiteSpace(Token):
 
 
 
+idempotent_tokens = {WhiteSpace}
 
 
 def eat(cls:Type[Token]):
@@ -40,11 +41,11 @@ def eat(cls:Type[Token]):
     Makes function return include constructor for token class that it tries to eat, in tupled with return.
     """
     assert issubclass(cls, Token), f"cls must be a subclass of Token, but got {cls}"
-    def decorator(eat_fn:Callable[[str], int|None]):
+    def decorator(eat_func:Callable[[str], int|None]):
         def wrapper(src:str) -> tuple[int|None, Type[Token]]:
-            return eat_fn(src), cls
+            return eat_func(src), cls
         wrapper._is_eat_decorator = True  # make it easy to check if a function has this decorator
-        wrapper.eat_fn = eat_fn
+        wrapper.eat_func = eat_func
         return wrapper
     return decorator
 
@@ -89,16 +90,55 @@ def eat_block_comment(src:str) -> int|None:
 
 
 
-
-#TODO: maybe some use of sub-parsing where a sub-string is given, possibly not entirely parsed, and then the tokens is returned + the remaining string
-#      need to tell sub parser when to stop though, by passing some sort of closing token it's looking for, e.g. }, but at that point, why not just do everything with the context stack?
-def tokenize(src:str) -> list[Token]:
-    #get a list of all functions that are decorated with @eat
-    eat_funcs = get_eat_functions()
+def tokenize(src:str, context:list[Context]|None=None) -> list[Token]:
+    if context is None:
+        #top level tokenizing
+        context = [Context.root]
+        min_context = 0
+    else:
+        #sub tokenizing. Stop when we try to do anything involving the parents context
+        min_context = len(context)
     
-    context = [Context.root]
-    ...
+    #get a list of all functions that are decorated with @eat
+    eat_funcs = [func for _, func in get_eat_functions()]
 
+    tokens = []
+    i = 0
+
+    while i < len(src):
+        for eat_func in eat_funcs:
+            num_eaten, token_cls = eat_func(src[i:])
+            if num_eaten is not None:
+                if token_cls not in idempotent_tokens or not tokens or not isinstance(tokens[-1], token_cls):
+                    tokens.append(token_cls(src[i:i+num_eaten]))
+                i += num_eaten
+                break
+        else:
+            raise ValueError(f"failed to tokenize:\n{repr(src[i:])}")
+        
+        #check if we need to stop tokenizing
+        #TODO: this should go inside the loop?
+
+
+
+def get_eat_functions():
+    """Get a list of all functions decorated with @eat"""
+    return [(name, func) for name, func in globals().items() if callable(func) and hasattr(func, "_is_eat_decorator") and func._is_eat_decorator]
+
+def validate_functions():
+    # Get all functions decorated with @eat(Token)
+    decorated_functions = get_eat_functions()
+    print(decorated_functions)
+    # Validate the function signatures
+    for name, wrapper_func in decorated_functions:
+        func = wrapper_func.eat_func
+        signature = inspect.signature(func)
+        param_types = [param.annotation for param in signature.parameters.values()]
+        return_type = signature.return_annotation
+
+        # Check if the function has the correct signature
+        if len(param_types) != 1 or param_types[0] != str or return_type != int|None:
+            raise ValueError(f"{func.__name__} has an invalid signature: `{signature}`. Expected `(src: str) -> int|None`")
 
 
 
@@ -114,28 +154,9 @@ def test():
 
     1
 
-def get_eat_functions():
-    """Get a list of all functions decorated with @eat"""
-    return [(name, func) for name, func in globals().items() if callable(func) and hasattr(func, "_is_eat_decorator") and func._is_eat_decorator]
 
-def validate_functions():
-    # Get all functions decorated with @eat(Token)
-    decorated_functions = get_eat_functions()
-    print(decorated_functions)
-    # Validate the function signatures
-    for name, wrapper_func in decorated_functions:
-        func = wrapper_func.eat_fn
-        signature = inspect.signature(func)
-        param_types = [param.annotation for param in signature.parameters.values()]
-        return_type = signature.return_annotation
-
-        # Check if the function has the correct signature
-        if len(param_types) != 1 or param_types[0] != str or return_type != int|None:
-            raise ValueError(f"{func.__name__} has an invalid signature: `{signature}`. Expected `(src: str) -> int|None`")
-
-
-validate_functions()
 
 
 if __name__ == "__main__":
+    validate_functions()
     test()
