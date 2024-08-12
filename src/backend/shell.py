80006d88768951a4389319194c0bf43d1b5d916e
David Samson <david.andrew.engineer@gmail.com>
2024-08-12 01:45:20 -0400

    added note about where to find info on polyglot programs

--- src/backend/shell.py
+++ src/backend/shell.py
@@ -3,4 +3,5 @@ from pathlib import Path
 def shell_compiler(path: Path, args: list[str]):
     """this would target sh/powershell/etc. all simultaneously"""
     # TODO: find the explanation of how this works
+    # https://en.wikipedia.org/wiki/Polyglot_(computing)
     raise NotImplementedError('Shell backend is not yet supported')
