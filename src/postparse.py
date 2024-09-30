a815b29f08877447e6faa4aa1418a0dda468963b
David Samson <david.andrew.engineer@gmail.com>
2024-09-29 23:43:05 -0400

    created stub for converting prototype tuples into something concrete

--- src/postparse.py
+++ src/postparse.py
@@ -30,6 +30,7 @@ import pdb
 def post_parse(ast: AST) -> AST:
 
     ast = convert_prototype_identifiers(ast)
+    ast = convert_prototype_tuples(ast)
 
     # at the end of the post parse process
     if not ast.is_settled():
@@ -48,6 +49,8 @@ def convert_prototype_identifiers(ast: AST) -> AST:
             continue
 
         match i:
+            case PrototypeIdentifier(name=name):
+                gen.send(Express(Identifier(name)))
             case Call(f=PrototypeIdentifier(name=name), args=args):
                 gen.send(Call(Identifier(name), args))
             case Call(args=None): ...
@@ -58,8 +61,8 @@ def convert_prototype_identifiers(ast: AST) -> AST:
             case Assign():
                 pdb.set_trace()
                 ...
-            case PrototypeIdentifier(name=name):
-                gen.send(Express(Identifier(name)))
+            case IterIn(left=PrototypeIdentifier(name=name), right=right):
+                gen.send(IterIn(Identifier(name), right))
             case SequenceUnpackTarget():
                 pdb.set_trace()
                 ...
@@ -74,7 +77,7 @@ def convert_prototype_identifiers(ast: AST) -> AST:
                 ...
 
             # cases that themselves don't get adjusted but may contain nested children that need to be converted
-            case IString() | Group() | Block() | FunctionLiteral() | Loop() | Less() | Add():
+            case IString() | Group() | Block() | FunctionLiteral() | Loop() | Less() | Add() | Range() | Tuple():
                 ...
             # case Access() | Declare() | PointsTo() | BidirPointsTo() | Type() | ListOfASTs() | Tuple() | Block() | BareRange() | Ellipsis() | Spread() | Array() | Group() | Range() | Object() | Dict() | BidirDict() | TypeParam() | Void() | Undefined() | String() | IString() | Flowable() | Flow() | If() | Loop() | Default() | FunctionLiteral() | PrototypePyAction() | PyAction() | Call() | Index() | PrototypeIdentifier() | Express() | Identifier() | TypedIdentifier() | TypedGroup() | SequenceUnpackTarget() | ObjectUnpackTarget() | Assign() | Int() | Bool() | Range() | IterIn() | Less() | LessEqual() | Greater() | GreaterEqual() | Equal() | MemberIn() | LeftShift() | RightShift() | LeftRotate() | RightRotate() | LeftRotateCarry() | RightRotateCarry() | Add() | Sub() | Mul() | Div() | IDiv() | Mod() | Pow() | And() | Or() | Xor() | Nand() | Nor() | Xnor() | Not() | UnaryPos() | UnaryNeg() | UnaryMul() | UnaryDiv() | DeclarationType() | DeclareGeneric() | Parameterize():
                 # ...
@@ -85,36 +88,21 @@ def convert_prototype_identifiers(ast: AST) -> AST:
 
     return ast.items[0]
 
-# TODO: this is pretty inefficient memory-wise. more ideal would be in place conversions
 
+def convert_prototype_tuples(ast: AST) -> AST:
+    ast = Group([ast])
+    for i in (gen := ast.__full_traversal_iter__()):
+        # if i.is_settled():
+        #     continue
 
-def OLD_convert_prototype_identifiers(ast: AST) -> AST:
-    match ast:
-        case PrototypeIdentifier(name=name):
-            return Express(Identifier(name))
-        case Call(f=PrototypeIdentifier(name=name), args=args):
-            return Call(Identifier(name), convert_prototype_identifiers(args) if args else None)
-        case Group(items=items):
-            return Group([convert_prototype_identifiers(i) for i in items])
-        case Block(items=items):
-            return Block([convert_prototype_identifiers(i) for i in items])
-        case Assign(left=PrototypeIdentifier(name=name), right=right):
-            return Assign(left=Identifier(name), right=convert_prototype_identifiers(right))
-        case FunctionLiteral(args=args, body=body):
-            return FunctionLiteral(convert_prototype_identifiers(args), convert_prototype_identifiers(body))
-        case Void():
-            return ast
-        case Undefined():
-            return ast
-        case String():
-            return ast
-        case IString():
-            return IString(parts=[convert_prototype_identifiers(i) for i in ast.parts])
-        case _:
-            raise NotImplementedError(f'conversion not handled for {type(ast)}')
-    for i in ast:
-        print(repr(i))
+        match i:
+            case Range(left=Tuple(items=start_items)):
+                pdb.set_trace()
+                ...
+            case Range(right=Tuple(items=end_items)):
+                pdb.set_trace()
+                ...
+            # case _:
+            #     raise ValueError(f'Unhandled case {type(i)}')
 
-    pdb.set_trace()
-
-    return ast
+    return ast.items[0]
