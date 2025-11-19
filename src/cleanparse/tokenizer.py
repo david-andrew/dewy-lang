aeea75640a9e70c230035a5876b0cff05d68d769
David Samson <david.andrew.engineer@gmail.com>
2025-11-19 02:20:52 -0500

    added escape character tokenizer

--- src/cleanparse/tokenizer.py
+++ src/cleanparse/tokenizer.py
@@ -1,3 +1,11 @@
+"""
+[Tasks]
+- somehow determine context stack updates after eating a normal token
+- make eat checks try the context's close function
+- string escape tokens
+- eat all tokens in hello world program
+"""
+
 from .errors import Span, Error, SrcFile, Pointer
 from .utils import truncate
 # from .meta import Tokenized, AST
@@ -33,6 +41,38 @@ class BlockBody(Context):
 
 
 
+
+# TODO: expand the list of valid identifier characters
+digits = set('0123456789')
+alpha = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
+greek = set('ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρςστυφχψω')
+misc = set('_?!$&°')
+start_characters = (alpha | greek | misc)
+continue_characters = (alpha | digits | greek | misc)
+
+# note that the prefix is case insensitive, so call .lower() when matching the prefix
+# numbers may have _ as a separator (if _ is not in the set of digits)
+number_bases = {
+    '0b': {*'01'},  # binary
+    '0t': {*'012'},  # ternary
+    '0q': {*'0123'},  # quaternary
+    '0s': {*'012345'},  # seximal
+    '0o': {*'01234567'},  # octal
+    '0d': {*'0123456789'},  # decimal
+    '0z': {*'0123456789xeXE'},  # dozenal
+    '0x': {*'0123456789abcdefABCDEF'},  # hexadecimal
+    '0u': {*'0123456789abcdefghijklmnopqrstuvABCDEFGHIJKLMNOPQRSTUV'},  # base 32 (duotrigesimal)
+    '0r': {*'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'},  # base 36 (hexatrigesimal)
+    '0y': {*'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$'},  # base 64 (tetrasexagesimal)
+}
+
+def is_based_digit(digit: str, base: str) -> bool:
+    """determine if a digit is valid in a given base"""
+    digits = number_bases[base]
+    return digit in digits
+
+
+
 @dataclass
 class Token(ABC):
     src: str
@@ -46,15 +86,6 @@ class Token(ABC):
     def eat(src:str, ctx:Context) -> int|None:
         """Try to eat a token, return the number of characters eaten or None"""
 
-
-
-# TODO: expand the list of valid identifier characters
-digits = set('0123456789')
-alpha = set('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
-greek = set('ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρςστυφχψω')
-misc = set('_?!$&°')
-start_characters = (alpha | greek | misc)
-continue_characters = (alpha | digits | greek | misc)
 
 
 class Identifier(Token):
@@ -110,7 +141,62 @@ class StringChars(Token):
         
         return i or None
 
-# class StringEscape(Token): ...
+class StringEscape(Token):
+    # TODO: Note there is a current gap in constructable strings with escapes.
+    # e.g. my_str = 'something \u38762<something>'
+    # any hex characters [0-9a-fA-F] cannot be in <something> because they are just consumed by the unicode codepoint
+    # specifically \a \b \f \0 are all problems because since those are escaped, we can't escape to get the character itself
+    # the possible solution is to introduce another escape character that puts nothing, and acts just as a delimiter
+    # e.g. my_str = 'something \u38762\ <something>' // or 'something \u38762\d<something>' or etc.
+    # `\ ` is interesting because space probably never needs to be literally delimited
+    @staticmethod
+    def eat(src:str, ctx:Context) -> int|None:
+        r"""
+        Eat an escape sequence, return the number of characters eaten
+        Escape sequences must be either a known escape sequence:
+        - \n newline
+        - \r carriage return
+        - \t tab
+        - \b backspace
+        - \f form feed
+        - \v vertical tab
+        - \a alert
+        - \0 null
+        - \u##..# or \U##..# for an arbitrary unicode character. May have any number of hex digits
+        - (TODO) `\ ` (slash-space) for delimiting the end of a unicode codepoint, so the following character isn't consumed by it
+
+        or a \ followed by an unknown character. In this case, the escape converts to just the unknown character
+        This is how to insert characters that are otherwise illegal inside a string, e.g.
+        - \' converts to just a single quote '
+        - \{ converts to just a single open brace {
+        - \\ converts to just a single backslash \
+        - \m converts to just a single character m
+        - etc.
+        """
+        if not isinstance(ctx, StringBody):
+            raise ValueError("INTERNAL ERROR: attempted to eat StringEscape in a non-string context")
+
+        if not src.startswith('\\'):
+            return None
+
+        if len(src) == 1:
+            raise ValueError("unterminated escape sequence")
+
+        if src[1] in 'uU':
+            i = 2
+            while i < len(src) and is_based_digit(src[i], '0x'):
+                i += 1
+            if i == 2:
+                raise ValueError("invalid unicode escape sequence")
+            return i
+
+        # if src[1] in 'nrtbfva0':
+        #     return 2
+
+        # all other escape sequences (known or unknown) are just a single character
+        return 2
+
+
 # class BlockOpen(Token): ...
 # class BlockClose(Token): ...
 # class WhiteSpace(Token): ...
