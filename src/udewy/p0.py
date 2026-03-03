b8a3eef57c388cb4d8b6de26717255187eb7d7e6
David Samson <david.andrew.engineer@gmail.com>
2026-03-03 00:58:10 -0500

    stubbed out how udewy parser will look

--- src/udewy/p0.py
+++ src/udewy/p0.py
@@ -17,12 +17,25 @@ def parse(toks:list[t0.PackedToken], backend=x84_64) -> str:
     # minor complexity around throwing an error for expressions that increase in precedence level from left to right
     pdb.set_trace()
 
-def parse_program(toks: list[t0.PackedToken], scope:Scope, fn_decls:list[FnDecl]):
+def parse_program(toks: list[t0.PackedToken], src:str, scope:Scope, fn_decls:list[FnDecl]):
     while len(toks) > 0:
         parse_statement(toks)
 
-def parse_statement(toks: list[t0.PackedToken]):
-    ...
+def parse_statement(toks: list[t0.PackedToken], src:str, scope:Scope, fn_decls:list[FnDecl]):
+    if t0.kindof(toks[0]) == t0.TK_LET:
+        parse_let_statement(toks, src, scope, fn_decls)
+    elif t0.kindof(toks[0]) == t0.TK_IF:
+        parse_if_statement(toks, src, scope, fn_decls)
+    elif t0.kindof(toks[0]) == t0.TK_LOOP:
+        parse_loop_statement(toks, src, scope, fn_decls)
+    elif t0.kindof(toks[0]) == t0.TK_RETURN:
+        parse_return_statement(toks, src, scope, fn_decls)
+    elif t0.kindof(toks[0]) == t0.TK_BREAK:
+        parse_break_statement(toks, src, scope, fn_decls)
+    elif t0.kindof(toks[0]) == t0.TK_CONTINUE:
+        parse_continue_statement(toks, src, scope, fn_decls)
+    else: 
+        parse_expression(toks, src, scope, fn_decls)
 
 if __name__ == "__main__":
     import sys
--- src/udewy/t0.py
+++ src/udewy/t0.py
@@ -75,13 +75,16 @@ FALSE_VALUE: TokenValue = 0
 NO_VALUE: TokenValue = 0 # i.e. placeholder for tokens that don't have a value
 
 
-def pack_token(kind:TokenKind, value:TokenValue, location:Location)->PackedToken:
+def pack(kind:TokenKind, value:TokenValue, location:Location)->PackedToken:
     """Pack a token into a single 64-bit integer"""
     return (kind << 32) | (value << 16) | location
 
-def unpack_token(packed:PackedToken)->Token:
+def unpack(packed:PackedToken)->Token:
     """Unpack a token from a single 64-bit integer"""
     return (packed >> 32, (packed >> 16) & 0xFFFF, packed & 0xFFFF)
+
+def kindof(packed:PackedToken)->TokenKind:
+    return packed >> 32
 
 def str_left_eq(prefix:str, s:str)->bool:
     """Check if the prefix is a left substring of the string"""
@@ -116,7 +119,7 @@ def tokenize(src:str)->list[PackedToken]:
     while i < n:
         # running sanity check(s) for prototype tokens that shouldn't be in the final output
         # check here so that we can maintain a single pass tokenizer
-        if len(toks) > 1 and (unpack_token(toks[-2])[0] == _TK_COLON or unpack_token(toks[-2])[0] == _TK_FN_COLON):
+        if len(toks) > 1 and (kindof(toks[-2]) == _TK_COLON or kindof(toks[-2]) == _TK_FN_COLON):
             raise SyntaxError(f"colon must be followed by a type annotation at {i}: {src[i]!r}")
         
         # current character
@@ -143,49 +146,49 @@ def tokenize(src:str)->list[PackedToken]:
             text = src[start:i]
 
             if str_left_eq("let", text) or str_left_eq("const", text):
-                toks.append(pack_token(TK_LET, NO_VALUE, start))
+                toks.append(pack(TK_LET, NO_VALUE, start))
             elif str_left_eq("and", text):
-                toks.append(pack_token(TK_AND, NO_VALUE, start))
+                toks.append(pack(TK_AND, NO_VALUE, start))
             elif str_left_eq("or", text):
-                toks.append(pack_token(TK_OR, NO_VALUE, start))
+                toks.append(pack(TK_OR, NO_VALUE, start))
             elif str_left_eq("not", text):# and (i >= n or src[i] != '='):  # one special case since `not=?` is checked after
-                toks.append(pack_token(TK_NOT, NO_VALUE, start))
+                toks.append(pack(TK_NOT, NO_VALUE, start))
             elif str_left_eq("true", text):
-                toks.append(pack_token(TK_NUMBER, TRUE_VALUE, start))
+                toks.append(pack(TK_NUMBER, TRUE_VALUE, start))
             elif str_left_eq("false", text):
-                toks.append(pack_token(TK_NUMBER, FALSE_VALUE, start))
+                toks.append(pack(TK_NUMBER, FALSE_VALUE, start))
             elif str_left_eq("if", text):
-                toks.append(pack_token(TK_IF, NO_VALUE, start))
+                toks.append(pack(TK_IF, NO_VALUE, start))
             elif str_left_eq("loop", text):
-                toks.append(pack_token(TK_LOOP, NO_VALUE, start))
+                toks.append(pack(TK_LOOP, NO_VALUE, start))
             elif str_left_eq("else", text):
-                toks.append(pack_token(TK_ELSE, NO_VALUE, start))
+                toks.append(pack(TK_ELSE, NO_VALUE, start))
             elif str_left_eq("return", text):
-                toks.append(pack_token(TK_RETURN, NO_VALUE, start))
+                toks.append(pack(TK_RETURN, NO_VALUE, start))
             elif str_left_eq("break", text):
-                toks.append(pack_token(TK_BREAK, NO_VALUE, start))
+                toks.append(pack(TK_BREAK, NO_VALUE, start))
             elif str_left_eq("continue", text):
-                toks.append(pack_token(TK_CONTINUE, NO_VALUE, start))
+                toks.append(pack(TK_CONTINUE, NO_VALUE, start))
             elif i < n and src[i] == '(':
-                toks.append(pack_token(TK_IDENT_CALL, len(text), start))
+                toks.append(pack(TK_IDENT_CALL, len(text), start))
                 i += 1 # consume the '('
             elif i < n and src[i] == '[':
-                toks.append(pack_token(TK_IDENT_INDEX, len(text), start))
+                toks.append(pack(TK_IDENT_INDEX, len(text), start))
                 i += 1 # consume the '['
             elif i < n and src[i] == '.':
-                toks.append(pack_token(TK_IDENT_DOT, len(text), start))
+                toks.append(pack(TK_IDENT_DOT, len(text), start))
                 i += 1 # consume the '.'
-            elif unpack_token(toks[-1])[0] == _TK_COLON:
+            elif kindof(toks[-1]) == _TK_COLON:
                 toks.pop()
-                toks.append(pack_token(TK_TYPE, len(text), start))
+                toks.append(pack(TK_TYPE, len(text), start))
                 continue
-            elif unpack_token(toks[-1])[0] == _TK_FN_COLON:
+            elif kindof(toks[-1]) == _TK_FN_COLON:
                 toks.pop()
-                toks.append(pack_token(TK_FN_TYPE, len(text), start))
+                toks.append(pack(TK_FN_TYPE, len(text), start))
                 continue
             else:
                 # regular identifier
-                toks.append(pack_token(TK_IDENT, len(text), start))
+                toks.append(pack(TK_IDENT, len(text), start))
             continue
 
         # hex number
@@ -197,7 +200,7 @@ def tokenize(src:str)->list[PackedToken]:
                 digit_val = ord(src[i]) - 48 if 48 <= ord(src[i]) <= 57 else ord(src[i]) - 55
                 val = val * 16 + digit_val
                 i += 1
-            toks.append(pack_token(TK_NUMBER, val, start))
+            toks.append(pack(TK_NUMBER, val, start))
             continue
         
         # binary number
@@ -208,7 +211,7 @@ def tokenize(src:str)->list[PackedToken]:
             while i < n and (src[i] == '0' or src[i] == '1'):
                 val = val * 2 + (ord(src[i]) - 48)
                 i += 1
-            toks.append(pack_token(TK_NUMBER, val, start))
+            toks.append(pack(TK_NUMBER, val, start))
             continue
 
         # number (decimal int)
@@ -218,7 +221,7 @@ def tokenize(src:str)->list[PackedToken]:
             while i < n and is_digit(src[i]):
                 val = val * 10 + (ord(src[i]) - 48)
                 i += 1
-            toks.append(pack_token(TK_NUMBER, val, start))
+            toks.append(pack(TK_NUMBER, val, start))
             continue
         
         # string
@@ -234,80 +237,76 @@ def tokenize(src:str)->list[PackedToken]:
                 raise SyntaxError(f"unterminated string at {i}: {src[start:i]!r}")
             i += 1
             # store the string text location + length
-            toks.append(pack_token(TK_STRING, i - start, start))
+            toks.append(pack(TK_STRING, i - start, start))
             continue
 
 
         # multi-character tokens
         if str_left_eq("//", src[i:]):
-            toks.append(pack_token(TK_IDIV, NO_VALUE, i))
+            toks.append(pack(TK_IDIV, NO_VALUE, i))
             i += 2
             continue
         if str_left_eq("=?", src[i:]):
-            if toks[-1] == TK_NOT:
+            if kindof(toks[-1]) == TK_NOT:
                 toks.pop()
-                toks.append(pack_token(TK_NOT_EQ, NO_VALUE, i))
+                toks.append(pack(TK_NOT_EQ, NO_VALUE, i))
             else:
-                toks.append(pack_token(TK_EQ, NO_VALUE, i))
+                toks.append(pack(TK_EQ, NO_VALUE, i))
             i += 2
             continue
-        # if str_left_eq("not=?", src[i:]):
-        #     toks.append(pack_token(TK_NOT_EQ, NO_VALUE, i))
-        #     i += 5
-        #     continue
         if str_left_eq(">=?", src[i:]):
-            toks.append(pack_token(TK_GT_EQ, NO_VALUE, i))
+            toks.append(pack(TK_GT_EQ, NO_VALUE, i))
             i += 3
             continue
         if str_left_eq("<=?", src[i:]):
-            toks.append(pack_token(TK_LT_EQ, NO_VALUE, i))
+            toks.append(pack(TK_LT_EQ, NO_VALUE, i))
             i += 3
             continue
         if str_left_eq(">?", src[i:]):
-            toks.append(pack_token(TK_GT, NO_VALUE, i))
+            toks.append(pack(TK_GT, NO_VALUE, i))
             i += 2
             continue
         if str_left_eq("<?", src[i:]):
-            toks.append(pack_token(TK_LT, NO_VALUE, i))
+            toks.append(pack(TK_LT, NO_VALUE, i))
             i += 2
             continue
         if str_left_eq(":>", src[i:]):
-            toks.append(pack_token(_TK_FN_COLON, NO_VALUE, i))
+            toks.append(pack(_TK_FN_COLON, NO_VALUE, i))
             i += 2
             continue
         if str_left_eq("=>", src[i:]):
-            toks.append(pack_token(TK_FN_ARROW, NO_VALUE, i))
+            toks.append(pack(TK_FN_ARROW, NO_VALUE, i))
             i += 2
             continue
         if str_left_eq("|>", src[i:]):
-            toks.append(pack_token(TK_PIPE, NO_VALUE, i))
+            toks.append(pack(TK_PIPE, NO_VALUE, i))
             i += 2
             continue
         if str_left_eq("<<", src[i:]):
-            toks.append(pack_token(TK_LEFT_SHIFT, NO_VALUE, i))
+            toks.append(pack(TK_LEFT_SHIFT, NO_VALUE, i))
             i += 2
             continue
         if str_left_eq(">>", src[i:]):
-            toks.append(pack_token(TK_RIGHT_SHIFT, NO_VALUE, i))
+            toks.append(pack(TK_RIGHT_SHIFT, NO_VALUE, i))
             i += 2
             continue
         
 
         # single-character tokens
         i += 1
-        if c == "+": toks.append(pack_token(TK_PLUS, NO_VALUE, i));          continue
-        if c == "-": toks.append(pack_token(TK_MINUS, NO_VALUE, i));         continue
-        if c == "*": toks.append(pack_token(TK_MUL, NO_VALUE, i));           continue
-        if c == "%": toks.append(pack_token(TK_MOD, NO_VALUE, i));           continue
-        if c == "~": toks.append(pack_token(TK_BIT_NOT, NO_VALUE, i));       continue
-        if c == "(": toks.append(pack_token(TK_LEFT_PAREN, NO_VALUE, i));    continue
-        if c == ")": toks.append(pack_token(TK_RIGHT_PAREN, NO_VALUE, i));   continue
-        if c == "{": toks.append(pack_token(TK_LEFT_BRACE, NO_VALUE, i));    continue
-        if c == "}": toks.append(pack_token(TK_RIGHT_BRACE, NO_VALUE, i));   continue
-        if c == "[": toks.append(pack_token(TK_LEFT_BRACKET, NO_VALUE, i));  continue
-        if c == "]": toks.append(pack_token(TK_RIGHT_BRACKET, NO_VALUE, i)); continue
-        if c == "=": toks.append(pack_token(TK_ASSIGN, NO_VALUE, i));        continue
-        if c == ":": toks.append(pack_token(_TK_COLON, NO_VALUE, i));        continue
+        if c == "+": toks.append(pack(TK_PLUS, NO_VALUE, i));          continue
+        if c == "-": toks.append(pack(TK_MINUS, NO_VALUE, i));         continue
+        if c == "*": toks.append(pack(TK_MUL, NO_VALUE, i));           continue
+        if c == "%": toks.append(pack(TK_MOD, NO_VALUE, i));           continue
+        if c == "~": toks.append(pack(TK_BIT_NOT, NO_VALUE, i));       continue
+        if c == "(": toks.append(pack(TK_LEFT_PAREN, NO_VALUE, i));    continue
+        if c == ")": toks.append(pack(TK_RIGHT_PAREN, NO_VALUE, i));   continue
+        if c == "{": toks.append(pack(TK_LEFT_BRACE, NO_VALUE, i));    continue
+        if c == "}": toks.append(pack(TK_RIGHT_BRACE, NO_VALUE, i));   continue
+        if c == "[": toks.append(pack(TK_LEFT_BRACKET, NO_VALUE, i));  continue
+        if c == "]": toks.append(pack(TK_RIGHT_BRACKET, NO_VALUE, i)); continue
+        if c == "=": toks.append(pack(TK_ASSIGN, NO_VALUE, i));        continue
+        if c == ":": toks.append(pack(_TK_COLON, NO_VALUE, i));        continue
         i -= 1  # undo the increment above
 
 
@@ -329,7 +328,7 @@ def kind_to_str(kind:TokenKind)->str:
     raise ValueError(f"unknown token kind: {kind}")
 
 def dump_token(token:PackedToken, src:str):
-    kind, value, location = unpack_token(token)
+    kind, value, location = unpack(token)
     kind_str = kind_to_str(kind)
 
     if kind == TK_IDENT:
